442159a9f018c60153de1b66af7a47d0bd0837c9
Victor Pegoraro <63164117+victorpegoraro@users.noreply.github.com>
2024-04-22 17:16:39 -0300

    Update main.py
    Add validate CNPJ , CNS ( Cartão nacional do SUS )

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse, HTMLResponse
-from validate_docbr import CPF, CNH
+from validate_docbr import CPF, CNH, CNPJ, CNS
 
-app = FastAPI(title="Validate API", version="0.1" )
+app = FastAPI(title="Validate API", version="0.1", summary="API to validate brazilian documents" )
 
 
 @app.get("/")
 def read_root():
     #return {"DOCS": "https://validate-api.onrender.com/docs"}
-    return RedirectResponse("https://validate-api.onrender.com/docs", status_code=303)
+    return RedirectResponse("https://validate-api.onrender.com/docs", status_code=200)
 
 
 @app.get("/validate-cpf/{number}")
@@ -32,3 +32,25 @@ def validate_cpf(number: Union[str, None] = None):
         number = cnh.mask( number )
 
     return {"validate": validate, "doc": number, 'doc_type' : 'CNH'}
+
+
+@app.get("/validate-cns/{number}")
+def validate_cnpj(number: Union[str, None] = None):
+
+    cnpj = CNPJ()
+    validate = cnpj.validate(number)
+    if( validate ):
+        number = cnpj.mask( number )
+
+    return {"validate": validate, "doc": number, 'doc_type' : 'CNPJ'}
+
+
+@app.get("/validate-cns/{number}")
+def validate_cnpj(number: Union[str, None] = None):
+
+    cns = CNS()
+    validate = cns.validate(number)
+    if( validate ):
+        number = cns.mask( number )
+
+    return {"validate": validate, "doc": number, 'doc_type' : 'CNS'}
