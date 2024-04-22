80b5eff5208dbcb6b470f9df41ad3179adb56284
Victor Pegoraro <63164117+victorpegoraro@users.noreply.github.com>
2024-04-22 15:38:56 -0300

    Update main.py
    Add function to validate cpf

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ from typing import Union
 
 from fastapi import FastAPI
 
+from fastapi.responses import RedirectResponse, HTMLResponse
+
+from cpf import cpf_validate
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    #return {"DOCS": "https://validate-api.onrender.com/docs"}
+    return RedirectResponse("https://validate-api.onrender.com/docs", status_code=303)
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/cpf/{cpf}")
+def read_item(cpf: Union[str, None] = None):
+
+    validate = cpf_validate( cpf )
+
+    return {"validate": validate, "doc": cpf}
