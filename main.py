ceaae35ebf29799b710a4f34943e10593b250e06
Victor Pegoraro <63164117+victorpegoraro@users.noreply.github.com>
2024-04-22 18:08:32 -0300

    Update main.py
    Add card brand checker

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Union
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse, HTMLResponse
 from validate_docbr import CPF, CNH, CNPJ, CNS
+import re
 
 # Config FastApi
 tags_metadata = [
@@ -9,14 +10,14 @@ tags_metadata = [
         "name": "Validate number",
         "description": "Logical validation of document numbers",
     },
-    # {
-    #     "name": "items",
-    #     "description": "Manage items. So _fancy_ they have their own docs.",
-    #     "externalDocs": {
-    #         "description": "Items external docs",
-    #         "url": "https://fastapi.tiangolo.com/",
-    #     },
-    # },
+    {
+        "name": "Validate card",
+        "description": "Validate card informations",
+        # "externalDocs": {
+        #     "description": "Items external docs",
+        #     "url": "https://fastapi.tiangolo.com/",
+        # },
+    },
 ]
 
 # Start FastApi
@@ -69,3 +70,31 @@ def validate_cns(number: Union[str, None] = None):
         number = cns.mask( number )
 
     return {"validate": validate, "doc": number, 'doc_type' : 'CNS'}
+
+@app.get("/card-brand/{number}", tags=["Validate card"])
+def card_brand(number: Union[str, None] = None):
+    # Brands regex
+    brands = {
+        'visa': r'^4\d{12}(\d{3})?$',
+        'mastercard': r'^(5[1-5]\d{4}|677189)\d{10}$',
+        'diners': r'^3(0[0-5]|[68]\d)\d{11}$',
+        'discover': r'^6(?:011|5[0-9]{2})[0-9]{12}$',
+        'elo': r'^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})$',
+        'amex': r'^3[47]\d{13}$',
+        'jcb': r'^(?:2131|1800|35\d{3})\d{11}$',
+        'aura': r'^((?!504175))^((?!5067))(^50[0-9])/', # '/^(5078\d{2})(\d{2})(\d{11})$/',
+        'hipercard': r'^(606282\d{10}(\d{3})?)|(3841\d{15})$',
+        'maestro': r'^(?:5[0678]\d\d|6304|6390|67\d\d)\d{8,15}$',
+    }
+
+    # Run test
+    card_number = number
+    validate = False
+    brand = None
+    for _brand, regex in brands.items():
+        if re.match(regex, card_number):
+            brand = _brand
+            validate = True
+            break
+
+    return {"validate": validate, "brand": brand}
